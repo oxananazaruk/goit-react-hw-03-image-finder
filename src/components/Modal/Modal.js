aee640c8a807a150ee8c04be8e30ddebab5bf809
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2023-11-12 14:03:16 +0200

    Update Modal.js

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,11 +1,23 @@
 import ReactModal from 'react-modal';
 
 const customStyles = {
+  overlay: {
+    position: 'fixed',
+    top: '0',
+    left: '0',
+    width: '100vw',
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+  },
   content: {
     top: '50%',
     left: '50%',
     right: 'auto',
     bottom: 'auto',
+    padding: '0',
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
     maxWidth: 'calc(100vw - 48px)',
